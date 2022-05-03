a3bae667c9bbf5be1feb4ff7e685179e2dc7752f
Beatris <beti16b@gmail.com>
2022-05-03 22:34:19 +0300

    Home page changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,7 +3,8 @@ import './Home.css'
 function Home() {
     return (
         <div className="home-title">
-            <h1>Welcome to my PORTFOLIO</h1>
+            <h1>“I'm not a great programmer; <br /> I'm just a good programmer with great habits.” <br /> </h1>
+            <div id='author'>- Kent Beck</div>
             <div>
                 <span className='custom-icon'>&lt;</span>
                 <span className='custom-icon'>/</span>
