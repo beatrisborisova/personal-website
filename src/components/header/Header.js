bbc96d4175f2ac423801a49f324aaeea50c87694
Beatris <beti16b@gmail.com>
2022-06-09 21:49:17 +0300

    Nav changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,15 +5,15 @@ function Header() {
 
     return (
         <div className="header">
-                <nav className="nav-content">
-                    <NavLink to='/'>Home</NavLink>
-                    <NavLink to='projects'>Projects</NavLink>
-                    <NavLink to='about'>Contacts</NavLink>
-                </nav>
+            <nav className="nav-content">
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='projects'>Projects</NavLink>
+                <NavLink to='about'>Contacts</NavLink>
+            </nav>
+            <div className='menu'>Menu</div>
             <div className="logo-image-wrapper">
-                <img src="front-end-developer.png" alt='front-end-developer-logo'/>
+                <img src="front-end-developer.png" alt='front-end-developer-logo' />
             </div>
-
         </div>
     )
 }
