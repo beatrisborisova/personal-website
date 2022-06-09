5960af60e1145c0e54b8ebe508bf5437a061f27f
Beatris <beti16b@gmail.com>
2022-06-09 21:07:50 +0300

    Design changes

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,53 +3,51 @@ import './Projects.css'
 function Projects() {
 
     return (
-        <div className="projects-main">
-            <div className="content">
-                <h1>My projects</h1>
-                <div className='projects-container'>
-                    <a href="https://github.com/beatrisborisova/Books-Place" className="project">
-                        <div id='info'>
-                            <h3>Books Place</h3>
-                            <div className="project-image-wrapper">
-                                <img src='books-place.png' alt="project" />
-                            </div>
-                            <p>Books Place is an Angular project for books choosing a book by its resume.</p>
+        <div className="content">
+            <h1>My projects</h1>
+            <div className='projects-container'>
+                <a href="https://github.com/beatrisborisova/Books-Place" className="project">
+                    <div id='info'>
+                        <h3>Books Place</h3>
+                        <div className="project-image-wrapper">
+                            <img src='books-place.png' alt="project" />
                         </div>
-                        <div className='show-more'>View source code</div>
-                    </a>
-                    <a href="https://github.com/beatrisborisova/simple-battle-game" className="project">
-                        <div id='info'>
-                            <h3>Simple Battle Game</h3>
-                            <div className="project-image-wrapper">
-                                <img src='battle-game.png' alt="project" />
-                            </div>
-                            <p>Simple JavaScript application simulating a battle between two players.</p>
-                        </div>
-                        <div className='show-more'>View source code</div>
-                    </a>
-                    <a href="https://github.com/beatrisborisova/JavaScript---Music-App" className="project">
-                        <div id='info'>
-                            <h3>Music Player</h3>
-                            <div className="project-image-wrapper">
-                                <img src='music-player.png' alt="project" />
-                            </div>
-                            <p>Vanilla Javascript front-end application (SPA) for managing the music. </p>
-                        </div>
-                        <div className='show-more'>View source code</div>
-                    </a>
-                    <div className="project">
-                        <div id='info'>
-                            <h3>ReactJS Application</h3>
-                            <div className="project-image-wrapper">
-                                <img src='comming-soon.jpg' alt="comming-soon" />
-                            </div>
-                            <p>A cool ReactJS application now in a process of developing.</p>
-                        </div>
-                        {/* <div className='show-more'>View source code</div> */}
+                        <p>Books Place is an Angular project for books choosing a book by its resume.</p>
                     </div>
+                    <div className='show-more'>View source code</div>
+                </a>
+                <a href="https://github.com/beatrisborisova/simple-battle-game" className="project">
+                    <div id='info'>
+                        <h3>Simple Battle Game</h3>
+                        <div className="project-image-wrapper">
+                            <img src='battle-game.png' alt="project" />
+                        </div>
+                        <p>Simple JavaScript application simulating a battle between two players.</p>
+                    </div>
+                    <div className='show-more'>View source code</div>
+                </a>
+                <a href="https://github.com/beatrisborisova/JavaScript---Music-App" className="project">
+                    <div id='info'>
+                        <h3>Music Player</h3>
+                        <div className="project-image-wrapper">
+                            <img src='music-player.png' alt="project" />
+                        </div>
+                        <p>Vanilla Javascript front-end application (SPA) for managing the music. </p>
+                    </div>
+                    <div className='show-more'>View source code</div>
+                </a>
+                <div className="project">
+                    <div id='info'>
+                        <h3>ReactJS Application</h3>
+                        <div className="project-image-wrapper">
+                            <img src='comming-soon.jpg' alt="comming-soon" />
+                        </div>
+                        <p>A cool ReactJS application now in a process of developing.</p>
+                    </div>
+                    {/* <div className='show-more'>View source code</div> */}
                 </div>
-
             </div>
+
         </div>
     )
 }
